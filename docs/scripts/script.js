cb3fb9abe99fb7d21c6d1fbd581fa55b9ec7a602
chongzhi.ldq <chongzhi.ldq@taobao.com>
2016-03-15 21:05:41 +0800

    dev

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -2,13 +2,22 @@ require(['jquery'], function($) {
   $(function() {
     var headerHeight = $('header').outerHeight()
 
-    $(document).on('scroll.sidescoll', function(e) {
+    var setSideFixed = function() {
       if ($('body').scrollTop() > headerHeight) {
         $('.side').addClass('side-fixed')
       } else {
         $('.side').removeClass('side-fixed')
       }
+    }
 
+    $(document).on('scroll.sidescoll', setSideFixed)
+    setSideFixed()
+
+
+    $('.side').on('click', 'a', function(e) {
+      $('.side a').removeClass('active')
+      $(e.currentTarget).addClass('active')
     })
+
   })
 })
